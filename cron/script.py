0d23a91901c1a044639ef594318c0ce93175cff5
CY <iamCristYE@Gmail.com>
2024-09-18 23:44:25 +0800

    T

--- cron/script.py
+++ cron/script.py
@@ -94,6 +94,10 @@ for i in range(850, 1150):
     # Check if the content size is greater than 2KB (2048 bytes)
     if content_size > 2048:
         sendTelegramMsg(url)
+        for i in range(230, 255):
+            sendTelegramMsg(
+                f"https://vod-cdn0.lemino.docomo.ne.jp/img/{cid}/thumbnail/0{i}.jpg"
+            )
         break
     else:
         print("Size is smaller than 2KB.")
